1bae4ca16dd1e394aff9f010f1e7107483304c83
alexdevfreak <alexdevfreak@gmail.com>
2025-09-29 23:34:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,34 @@
+# ==========================
+#  TELEGRAM BOT CONFIG
+# ==========================
+API_ID = 123456          # Replace with your API ID from my.telegram.org
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+# ==========================
+#  CUSTOM WELCOME MESSAGE
+# ==========================
+WELCOME_TEXT = """<a href='https://share.google/images/Lvy1Y3SfHHif5nVwA'></a>
+
+⬡ <b>MEGA Indian Pack 2</b>
+<blockquote>#MostDemanded #HandPicked</blockquote>
+━━━━━━━━━━━━━━━━━━━━━━
+‣ <i>Media :</i> 50,00,000+
+‣ <i>Length :</i> 1000hrs+
+‣ <i>Genres :</i> Etc,Etc,Etc
+━━━━━━━━━━━━━━━━━━━━━━
+
+<b>ONLY ON :</b> ₹499 ₹99
+<b>ONLY ON :</b> <s>₹499</s> <b>₹99</b>
+
+<blockquote><i>Note: It Contains Personally Watched And Paid Contents</i></blockquote>
+
+Hello {mention}!
+Welcome to {title}!
+"""
+
+# ==========================
+#  BUTTON CONFIG
+# ==========================
+BUTTON_URL = "http://t.me/alex_clb?&text=Rᴇǫᴜᴇsᴛɪɴɢ%20Pʀᴇᴍɪᴜᴍ%20Pᴀᴄᴋ%20%E2%82%B999%20Vɪᴘ2"
+BUTTON_TEXT = "💳 Pᴀʏ Nᴏᴡ"
